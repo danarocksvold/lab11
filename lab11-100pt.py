7d45883e0374629e2b7ae6552fd2ac8d4f778a9d
danarocksvold <danarocksvold@gmail.com>
2014-11-04 13:29:31 -0600

    100pt

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -11,11 +11,11 @@
 from Tkinter import *
 root = Tk()
 # Create the canvas widget
-drawpad = Canvas(root, width=800,height=600, background='white')
+drawpad = Canvas(root, width=800,height=600, background='black')
 drawpad.grid(row=0, column=0)
 
 # Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
+circle = drawpad.create_oval(10, 10, 50, 50, fill='maroon')
 direction = 1
 # Create our animation function
 def animate():
@@ -23,9 +23,9 @@ def animate():
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
     if x2 > drawpad.winfo_width(): 
-        direction = - 1
+        direction = -900
     elif x1 < 0:
-        direction = 1
+        direction = 10
     #Move our oval object by the value of direction
     drawpad.move(circle,direction,0)
     # Wait for 1 millisecond, then recursively call our animate function
--- lab11-85pt.py
+++ lab11-85pt.py
@@ -9,23 +9,23 @@
 from Tkinter import *
 root = Tk()
 # Create the canvas widget
-drawpad = Canvas(root, width=800,height=600, background='white')
+drawpad = Canvas(root, width=800,height=600, background='black')
 drawpad.grid(row=0, column=0)
 
 # Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
+circle = drawpad.create_oval(10, 10, 50, 50, fill='gold')
 direction = 1
 # Create our animation function
 def animate():
     global direction
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
-    if x2 > drawpad.winfo_width(): 
-        direction = - 1
-    elif x1 < 0:
-        direction = 1
+    if y2 > drawpad.winfo_height(): 
+        direction = - 5
+    elif y1 < 0:
+        direction = 5
     #Move our oval object by the value of direction
-    drawpad.move(circle,direction,0)
+    drawpad.move(circle,0,direction)
     # Wait for 1 millisecond, then recursively call our animate function
     drawpad.after(1, animate)
 
